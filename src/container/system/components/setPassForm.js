1cd4e1660b8603028966980d41036014b00da7d4
严俊东 <1172482914@qq.com>
2019-02-27 10:53:38 +0800

    修改密码接口对接

--- src/container/system/components/setPassForm.js
+++ src/container/system/components/setPassForm.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {Form, Input,Button} from 'antd';
+import { Form, Input, Button } from 'antd';
 import './setPassForm.scss'
 
 class SetPassForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      confirmDirty: false
+    }
+  }
+
+  componentDidMount(){
+    this.props.onRef(this)
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -10,87 +21,137 @@ class SetPassForm extends React.Component {
       if (!err) {
         this.props.onSubmit(values)
       }
-    });
+    })
   }
 
-  handleRest = ()=>{
+  handleRest = () => {
     this.props.form.resetFields();
+  }
+
+  /**
+   * 确认密码框失去焦点
+   */
+  handleConfirmBlur = (e) => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  }
+
+  /**
+   * 校验规则 。 6 ~ 20 位。字母与数字。
+   */
+  checkRole = (rule, value, callback) => {
+    var regex = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/
+    if(!regex.test(value)) {
+      callback('密码必须为6-20位，字母与数字结合')
+    } else {
+      callback()
+    }
+  }
+
+  /**
+   * 校验两次密码是否一致
+   */
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('newPassword')) {
+      callback('输入的两个密码不一致!');
+    } else {
+      callback();
+    }
+  }
+
+  /**
+   * 更新原密码内容，触发确认密码的校验
+   */
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
   }
 
   render() {
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
-        span:8
+        span: 4
       },
       wrapperCol: {
-        span:16
+        span: 20
       },
     };
     const formItemLayout_button = {
-      style:{
+      style: {
         display: 'flex',
-        justifyContent:'flex-end'
+        justifyContent: 'flex-end'
       }
     };
 
     return (
       <div className="SetPassForm" >
-          <Form onSubmit={this.handleSubmit} >
-              <Form.Item
-                {...formItemLayout}
-                label="原密码">
-                  {
-                    getFieldDecorator('OldPassword', {
-                    rules: [{
-                      required: true,
-                      message: '请输入原密码！',
-                    }],
-                  })(
-                    <Input type="text" autoComplete='off' placeholder="请输入原密码" />
-                  )}
-              </Form.Item>
+        <Form onSubmit={this.handleSubmit} >
+          <Form.Item
+            {...formItemLayout}
+            label="原密码">
+            {
+              getFieldDecorator('oldPassword', {
+                rules: [{
+                  required: true,
+                  message: '请输入原密码！',
+                }],
+              })(
+                <Input type="password" autoComplete='off' placeholder="6-20位，字母与数字结合，字母区分大小写" maxLength={20} />
+              )}
+          </Form.Item>
 
-              <Form.Item
-                {...formItemLayout}
-                label="新密码">
-                  {
-                    getFieldDecorator('password', {
-                    rules: [{
-                      required: true, message: '请输入新密码！',
-                    }]
-                  })(
-                    <Input type="text" autoComplete='off' placeholder="请输入新密码" />
-                  )}
-              </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            label="新密码">
+            {
+              getFieldDecorator('newPassword', {
+                rules: [{
+                  required: true, message: '请输入新密码！',
+                }, {
+                  validator: this.validateToNextPassword,
+                }, {
+                  validator: this.checkRole,
+                }]
+              })(
+                <Input type="password" autoComplete='off' placeholder="6-20位，字母与数字结合，字母区分大小写" />
+              )}
+          </Form.Item>
 
-              <Form.Item
-                {...formItemLayout}
-                label="确认密码">
-                  {
-                    getFieldDecorator('confirm', {
-                    rules: [{
-                      required: true, message: '请确认您的新密码！',
-                    }]
-                  })(
-                    <Input type="text" autoComplete='off'  placeholder="请确认您的新密码"  />
-                  )}
-              </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            label="确认密码">
+            {
+              getFieldDecorator('confirm', {
+                rules: [{
+                  required: true, message: '请确认您的新密码！',
+                }, {
+                  validator: this.compareToFirstPassword
+                }]
+              })(
+                <Input type="password" autoComplete='off' placeholder="6-20位，字母与数字结合，字母区分大小写" onBlur={this.handleConfirmBlur} />
+              )}
+          </Form.Item>
 
-                <Form.Item {...formItemLayout_button}>
-                  <Button
-                    style={{marginRight:16}}
-                    type="primary"
-                    htmlType="submit">
-                     提交
+          <Form.Item {...formItemLayout_button}>
+            <Button
+              style={{ marginRight: 16 }}
+              type="primary"
+              htmlType="submit"
+              loading={this.props.loading}>
+              提交
                   </Button>
-                  <Button
-                    type="primary"
-                    onClick={this.handleRest}>
-                     重新输入
+            <Button
+              type="primary"
+              onClick={this.handleRest}>
+              重新输入
                   </Button>
-                </Form.Item>
-          </Form> 
+          </Form.Item>
+        </Form>
       </div>);
   }
 }
--- src/container/system/setPass.js
+++ src/container/system/setPass.js
@@ -2,21 +2,49 @@ import React from 'react';
 import { Card } from 'antd';
 import Sider from '../../component/sider/sider'
 import SetPassForm from './components/setPassForm'
+import request from '../../lib/request'
 import './style/setPass.scss'
 
 export default class SetPass extends React.Component {
-  
-  onSubmit = (value)=>{
-    console.log(value)
+
+  state = {
+    submitLoding: false, // 加载中状态
+  }
+
+  onSubmit = (value) => {
+    this.setState({
+      submitLoding: true
+    })
+    let user = sessionStorage.getItem('info')
+    user = user ? JSON.parse(user) : {}
+    request({
+      url: '/webApi/base/user/updatePwd',
+      method: 'post',
+      data: {
+        userId: user.id,
+        ...value
+      }
+    }).then(res => {
+      this.child.handleRest()
+    }).catch(err => {
+      this.setState({
+        submitLoding: false
+      })
+    })
+
+  }
+
+  ref = (child) => {
+    this.child = child
   }
 
   render() {
     return (
       <div className="setPass">
-          <Sider history={this.props.history}/>
-          <Card title="修改密码" className="changePass">
-             <SetPassForm onSubmit={this.onSubmit}/>
-          </Card>
+        <Sider history={this.props.history} />
+        <Card title="修改密码" className="changePass">
+          <SetPassForm onSubmit={this.onSubmit} onRef={this.ref} loading={this.state.submitLoding} />
+        </Card>
       </div>
     );
   }
