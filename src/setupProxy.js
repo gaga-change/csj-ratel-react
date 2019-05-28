df7a12cab1b73cc51b0cb458f212de426b17ae02
严俊东 <1172482914@qq.com>
2019-05-28 09:22:40 +0800

    添加 proxy 配置文件

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const proxy = require("http-proxy-middleware");
+
+const ip = "http://192.168.2.167:8686" // 修浩
+// const ip = "http://172.16.81.26:8686" // 苗
+
+module.exports = function(app) {
+  app.use(
+    proxy(['/webApi', '/login'], { target: ip, changeOrigin: true })
+  )
+}
