c169ae65f23223137293e1321e261cf96bf0a78c
严俊东 <1172482914@qq.com>
2019-06-12 14:17:54 +0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require("http-proxy-middleware");
 
 // const ip = "http://192.168.2.167:8686" // 修浩
-const ip = "http://172.16.81.26:8686" // 苗
+// const ip = "http://172.16.81.26:8686" // 苗
+const ip = "http://172.16.81.219:8686" // 龙
 // const ip = "http://192.168.2.126:8686" // 雷
 // const ip = "http://testratel.csjmro.com" // 测试环境
 
