affa24d06c7c37cbd48068079c37e56c79fb65f6
严俊东 <1172482914@qq.com>
2019-09-17 08:56:16 +0800

    首页文案修改

--- src/container/home/components/config.js
+++ src/container/home/components/config.js
@@ -1,22 +1,22 @@
-export  const priceChange_config=[
+export const priceChange_config = [
   {
-    name: '本周入库',//标题
-    orderNumber_dataIndex:'inWarehouseOrderNum',//单数的索引  没有该字段则不展示该字段
-    orderNumber_Company:'单',//单数的单位   没有则默认为 单
-    orderPiece_dataIndex:'inWarehouseSkuNum',//件数的索引  没有该字段则不展示该字段
-    orderPiece_Company:'件',//件数的单位    没有则默认为 件
-    icon:'home_in'//icon的索引
+    name: '今日入库',//标题
+    orderNumber_dataIndex: 'inWarehouseOrderNum',//单数的索引  没有该字段则不展示该字段
+    orderNumber_Company: '单',//单数的单位   没有则默认为 单
+    orderPiece_dataIndex: 'inWarehouseSkuNum',//件数的索引  没有该字段则不展示该字段
+    orderPiece_Company: '件',//件数的单位    没有则默认为 件
+    icon: 'home_in'//icon的索引
   },
   {
-    name: '本周出库',
-    orderNumber_dataIndex:'outWarehouseOrderNum',
-    orderPiece_dataIndex:'outWarehouseSkuNum',
-    icon:'home_out'
+    name: '今日出库',
+    orderNumber_dataIndex: 'outWarehouseOrderNum',
+    orderPiece_dataIndex: 'outWarehouseSkuNum',
+    icon: 'home_out'
   },
   {
     name: '商品库存',
-    orderNumber_dataIndex:'skuStockNum',
-    orderNumber_Company:'件',
-    icon:'home_repo'
+    orderNumber_dataIndex: 'skuStockNum',
+    orderNumber_Company: '件',
+    icon: 'home_repo'
   },
 ]
