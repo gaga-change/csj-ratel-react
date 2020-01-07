7a9dcfc2c53e78d95955254bfc77f729d88dbedd
严俊东 <1172482914@qq.com>
2020-01-07 15:17:50 +0800

    报表链接添加 ownerCode

--- src/container/report/reportList.js
+++ src/container/report/reportList.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import { parse } from 'qs';
+import { connect } from 'react-redux'
 
 class ReportList extends React.Component {
 
+  state = {
+    ownerCode: ''
+  }
+
   componentDidMount() {
-    const _ = () => {
-      let { u } = parse(this.props.history.location.search.slice(1))
-      console.log(u)
-    }
-    _()
-    this.props.history.listen(() => {
-      _()
+    const { user } = this.props
+    const { ownerCode } = user || {}
+
+    this.setState({
+      ownerCode: (Math.random() + '').substr(-3) + ownerCode + (Math.random() + '').substr(-3)
     })
   }
 
-
-
   render() {
     const { location } = this.props
+    const { ownerCode } = this.state
     let { u } = parse(location.search.slice(1))
     return (<div style={{ width: '100%', height: '100%' }}>
       {/* {location.search} */}
-      <iframe title="报表"
-        src={`http://bi.csjmro.com/WebReport/ReportServer?reportlet=${u}`}
-        style={{ width: '100%', height: '100%', 'background-color': 'rgb(255, 255, 255)', border: 'none' }}
-      >
-      </iframe>
+      {
+        ownerCode && <iframe title="报表"
+          src={`http://bi.csjmro.com/WebReport/ReportServer?reportlet=${u}&ownerCode=${ownerCode}`}
+          style={{ width: '100%', height: '100%', 'backgroundColor': 'rgb(255, 255, 255)', border: 'none' }}
+        >
+        </iframe>
+      }
     </div>)
   }
 }
 
-export default ReportList
+const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(ReportList)
